1dd366011b5f5799b47d5abb96b372a9609c67be
Noah EH <noah.c.francis@gmail.com>
2020-12-30 21:42:58 -0500

    Completed module

--- ssg/site.py
+++ ssg/site.py
@@ -2,7 +2,6 @@ from pathlib import Path
 
 
 class Site:
-
     def __init__(self, source, dest):
         self.source = Path(source)
         self.dest = Path(dest)
@@ -16,7 +15,4 @@ class Site:
         for path in self.source.rglob("*"):
             if path.is_dir():
                 self.create_dir(path)
-
-
-
 
